17871893718f729ba0dd49f590e7bbbeff2bef66
sajinrajan <sajinrajan082@gmail.com>
2024-11-02 04:30:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// <!-- HEADER LOGIN AND NAVE OR LOGO MOBIL MENU BAR SECTION START -->
+
+const menuIcon = document.getElementById('menu-icon');
+const mobileDiv = document.getElementById('mobil-menu-bar');
+
+function openMenuBar() {
+    mobileDiv.classList.add('open');
+}
+
+function closeMenuBar() {
+    mobileDiv.classList.remove('open');
+}
+
+// <!-- HEADER LOGIN AND NAVE OR LOGO MOBIL MENU BAR SECTION END -->
+////////////////////////////////////////////////////////////////////
+// <!-- FIRST BODY SECTION TEXT OR CONTENT AND IMAGE SECTION START -->
+
+// <!-- FIRST BODY SECTION TEXT OR CONTENT AND IMAGE SECTION END -->
+////////////////////////////////////////////////////////////////
+// <!-- SECOND SECTION IN MY QUALITY AND SERVICE SECTION START -->
+
+//  <!-- SECOND SECTION IN MY QUALITY AND SERVICE SECTION END -->
+////////////////////////////////////////////////////////////////-->
+
+// <!-- THIRD SECTION IN MY RECENT WORK  SECTION START -->
+
+// <!-- THIRD SECTION IN MY RECENT WORK  SECTION END -->
+// <!-- //////////////////////////////////////////////////////////////////// -->
